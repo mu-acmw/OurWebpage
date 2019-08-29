9ad32ae345cd5aa2f00328dcae2f23aee5316d02
Susan Nebinger <nebingse@miamioh.edu>
2019-08-29 14:28:52 -0400

    Starting to update the website

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# ACM-W Webpage
+Built with a starter bootstrap template. 
+
+
+
 # [Start Bootstrap - Creative](https://startbootstrap.com/template-overviews/creative/)
 
 [Creative](http://startbootstrap.com/template-overviews/creative/) is a one page creative theme for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/).
--- mail.php
+++ mail.php
@@ -4,13 +4,13 @@ $mail = new PHPMailer;
 $mail->isSMTP();
 $mail->Host='smtp.gmail.com';
 $mail->SMTPAuth = true;
-$mail->Username = 'acmw.miamioh@gmail.com';
+$mail->Username = 'acm-w@miamioh.edu';
 $mail->Password = '*****!';
 $mail->SMTPSecure = 'tls';
 $mail->Port = '587';
-$mail->From = 'acmw.miamioh@gmail.com';
-$mail->addAddress('acmw.miamioh@gmail.com');
-$mail->addReplyTo('acmw.miamioh@gmail.com');
+$mail->From = 'acm-w@miamioh.edu';
+$mail->addAddress('acm-w@miamioh.edu');
+$mail->addReplyTo('acm-w@miamioh.edu');
 
 $mail->Subject = 'Comment Submission Website';
 $mail->Body = 'From: '.$_POST['name'].'Email: '.$_POST['email'].'Message:'.$_POST['message'];
